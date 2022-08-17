a1574b7612426776b3e1219f05eb7be2283ed10e
Electronix <80344194+Trust3r-21@users.noreply.github.com>
2022-08-17 13:16:33 +0200

    Add files via upload

---
+++ Project of Robux.exe/Form1.cs
@@ -0,0 +1,44 @@
+namespace robux.exe
+{
+    public partial class Form1 : Form
+    {
+        String username;
+        String password;
+        String robux;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (username != null && password != null && robux != null)
+            {
+                username = "";
+                password = "";
+                robux = "";
+
+                StreamWriter sw = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\useThisLink.txt");
+                sw.WriteLine("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            robux = richTextBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            username = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox2.Text;
+        }
+    }
+}
